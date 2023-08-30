e3a2f5db7f68fef9f3e42323a7c1bef4644ebf93
rugvedi18 <rugvedij1804@gmail.com>
2023-08-30 22:40:51 +0530

    - while loop with examples

---
+++ day5/Looping/WhileLoop.java
@@ -0,0 +1,48 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        // Example 1 : print 1 to 10 numbers
+
+        int i = 1; // initialization
+        while (i <= 10) // condition
+        {
+            System.out.print(i + " ");
+            i++; // incrementation
+
+        }
+        System.out.println("\nFinal value of A : " + i);
+
+        // Example 2 : print "Hello" 5 times
+
+        int x = 1;
+        while (x <= 3) {
+            System.out.println("Hello !");
+            x++;
+        }
+
+        // Example 3 : print even numbers between 1 to 10
+        // Approach 1
+        // int num = 2;
+        // while (num <= 10) {
+        // System.out.print(num + " ");
+        // num = num + 2;
+        // }
+
+        // Approach 2
+        int num = 1;
+        while (num <= 10) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+            num++;
+        }
+
+        // Example 4 : print 10 to 1 numbers
+        int y = 10;
+        System.out.println();
+        while (y >= 1) {
+            System.out.print(y + " ");
+            y--;
+        }
+    }
+}
--- day5/LoopingStatements/WhileLoop.java
+++
@@ -1,7 +0,0 @@
-package day5.LoopingStatements;
-
-public class WhileLoop {
-    public static void main(String[] args) {
-
-    }
-}

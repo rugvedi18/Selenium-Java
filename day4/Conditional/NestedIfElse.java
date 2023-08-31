2e6cc951c1ab8e028a95b5ec04d23218338ffea0
rugvedi18 <rugvedij1804@gmail.com>
2023-08-31 15:00:19 +0530

    - conflict fix

--- day4/Conditional/NestedIfElse.java
+++ day4/Conditional/NestedIfElse.java
@@ -2,14 +2,15 @@ package day4.Conditional;
 
 public class NestedIfElse {
     public static void main(String[] args) {
-        if (false) {
-            if (false) {
+        boolean val = true;
+        if (val) {
+            if (!val) {
                 System.out.println("abc");
             } else {
                 System.out.println("xyz");
             }
         } else {
-            if (true) {
+            if (val) {
                 System.out.println("in else's true");
             } else {
                 System.out.println("123");

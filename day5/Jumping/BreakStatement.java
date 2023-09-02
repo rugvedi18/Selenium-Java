b64cd378e3cade8416b048a171522380001c720e
rugvedi18 <rugvedij1804@gmail.com>
2023-09-02 23:33:25 +0530

    - break continue added

---
+++ day5/Jumping/BreakStatement.java
@@ -0,0 +1,11 @@
+public class BreakStatement {
+    public static void main(String[] args) {
+        // Example 1 : Break statement
+        for (int i = 1; i <= 5; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}

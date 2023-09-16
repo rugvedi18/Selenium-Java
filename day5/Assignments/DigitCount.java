cae6d70a26894690188972c3f5224a9301eaed6a
Rugvedi Jamgaonkar <66817023+rugvedi18@users.noreply.github.com>
2023-09-16 21:14:34 +0530

    Create DigitCount.java

---
+++ day5/Assignments/DigitCount.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DigitCount {
+
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+
+        String str = String.valueOf(num);
+        int count = str.length();
+        System.out.println("Number of digits : " + count);
+
+        // int count = 0;
+        // while(num != 0) {
+        // num = num/10;
+        // ++count;
+        // }
+        // System.out.print("Number of digits : " + count)
+    }
+}

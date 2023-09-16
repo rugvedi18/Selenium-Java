106f7952ae2884155f248019dace44c703d373bd
Rugvedi Jamgaonkar <66817023+rugvedi18@users.noreply.github.com>
2023-09-16 22:43:40 +0530

    Create EvenOddCount.java

---
+++ day5/Assignments/EvenOddCount.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EvenOddCount {
+
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+
+        int even_count = 0;
+        int odd_count = 0;
+
+        while(num > 0) {
+            if(num % 2 == 0) {
+                even_count++;
+            } else {
+                odd_count++;
+            }
+            num = num/10;
+        }
+
+        System.out.println("Number of even digits : " + even_count);
+        System.out.print("Number of odd digits : " + odd_count);
+    }
+}

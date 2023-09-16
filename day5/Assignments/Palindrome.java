e6fccafe4c7aa79c3bb782042c7cc39e2dfb6a8d
Rugvedi Jamgaonkar <66817023+rugvedi18@users.noreply.github.com>
2023-09-16 10:13:38 +0530

    Create Palindrome.java

---
+++ day5/Assignments/Palindrome.java
@@ -0,0 +1,31 @@
+package day5.Assignments;
+
+import java.util.Scanner;
+public class Palindrome {
+
+    public static void main(String []args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+        int number1 = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int lastDigit = number % 10;
+            reversedNumber = reversedNumber * 10 + lastDigit;
+            number = number / 10;
+        }
+
+        if(reversedNumber == number1) {
+
+            System.out.println("Palindrome number");
+        } else {
+            // System.out.println(reversedNumber + " " + number1);
+            System.out.println("Not palindrome");
+        }
+
+        sc.close();
+    }
+}

ab6a30921b5400a46f5e2a2dc99fd575e5959795
rugvedi18 <rugvedij1804@gmail.com>
2023-09-17 18:11:42 +0530

    - create singledimarray

---
+++ day6/SingleDimArray.java
@@ -0,0 +1,28 @@
+/*  
+1) Declare an array
+2) Insert values into array
+30 Find size of an array
+4) Read single value from an array
+5) Read multiple values from an array  */
+
+public class SingleDimArray {
+    public static void main(String[] args) {
+        /*
+         * // Approach 1
+         * 
+         * // declaration
+         * int a[] = new int[3];
+         * // insert values
+         * a[0] = 100;
+         * a[1] = 200;
+         * a[2] = 300;
+         */
+
+        // Approach 2
+        int a[] = { 100, 200, 300 };
+
+        // size of an array
+        System.out.println(a.length);
+
+    }
+}

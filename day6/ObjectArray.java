63e1b732d046a50e971e78a407d43ad33d7b06fc
rugvedi <rugvedij1804@gmail.com>
2023-10-03 00:16:27 +0530

    - object array

---
+++ day6/ObjectArray.java
@@ -0,0 +1,21 @@
+public class ObjectArray {
+    public static void main(String[] args) {
+        
+        // object type array can contain any type of data
+        Object a[] = {10, 10.5, "Welcome", true, 'A'};
+        
+        /*
+        for(int i=0; i < a.length; i++)
+        {
+            System.out.println(a[i]);
+        }
+        */
+
+        // enhanced for loop
+
+        for(Object x:a)
+        {
+            System.out.println(x);
+        }
+    }
+}

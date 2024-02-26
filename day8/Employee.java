b0da31bd86aef1a92fb8df68aee5ba783604c7d1
rugvedi <rugvedij1804@gmail.com>
2024-02-26 17:09:14 +0530

    -  java class file and Main file

--- day8/Employee.java
+++ day8/Employee.java
@@ -17,20 +17,21 @@ public class Employee {
     }
 
     // To execute the Java class, you need to have a main method.
-    public static void main(String[] args) {
-        Employee emp1 = new Employee();
-        emp1.eid = 101;
-        emp1.ename = "John";
-        emp1.esal = 50000;
-        emp1.job = "Engineer";
-        emp1.display();
+    // Main method is moved to EmployeeMain.java
+    // public static void main(String[] args) {
+    // Employee emp1 = new Employee();
+    // emp1.eid = 101;
+    // emp1.ename = "John";
+    // emp1.esal = 50000;
+    // emp1.job = "Engineer";
+    // emp1.display();
 
-        System.out.println("");
-        Employee emp2 = new Employee();
-        emp2.eid = 101;
-        emp2.ename = "John";
-        emp2.esal = 50000;
-        emp2.job = "Engineer";
-        emp2.display();
-    }
+    // System.out.println("");
+    // Employee emp2 = new Employee();
+    // emp2.eid = 101;
+    // emp2.ename = "John";
+    // emp2.esal = 50000;
+    // emp2.job = "Engineer";
+    // emp2.display();
+    // }
 }
---
+++ day8/EmployeeMain.java
@@ -0,0 +1,27 @@
+package day8;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        emp1.eid = 101;
+        emp1.ename = "John";
+        emp1.esal = 50000;
+        emp1.job = "Engineer";
+        emp1.display();
+
+        System.out.println("");
+        Employee emp2 = new Employee();
+        emp2.eid = 101;
+        emp2.ename = "John";
+        emp2.esal = 50000;
+        emp2.job = "Engineer";
+        emp2.display();
+    }
+}
+
+// To run EmployeeMian.java, first you need to compile both files on root folder
+// C:\Users\Rugvedi\Desktop\RJ\Study\AutomationTesting\Selenium-Automation>
+// javac day8/Employee.java day8/EmployeeMain.java
+// class files will be created for both the files then run
+// C:\Users\Rugvedi\Desktop\RJ\Study\AutomationTesting\Selenium-Automation>
+// java day8.EmployeeMain

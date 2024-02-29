d2f9fe76d4467e9ed376f7edc09d7d8816668b93
rugvedi <rugvedij1804@gmail.com>
2024-02-29 23:21:07 +0530

    - default & parameterized constructor

---
+++ day9/ConstructorDemo.java
@@ -0,0 +1,35 @@
+package day9;
+
+public class ConstructorDemo {
+
+    int x, y;
+    String s;
+
+    // Default constructor
+    // ConstructorDemo() {
+    // x = 100;
+    // y = 200;
+    // s = "Default Constructor";
+    // }
+
+    // Parameterized constructor
+    ConstructorDemo(int a, int b, String str) {
+        x = a;
+        y = b;
+        s = str;
+    }
+
+    void DisplayData() {
+        System.out.println(x + " " + y + " " + s);
+    }
+
+    public static void main(String[] args) {
+        // Default cconstructor
+        // ConstructorDemo d1 = new ConstructorDemo();
+        // d1.DisplayData();
+
+        // Parameterized Constructor
+        ConstructorDemo d1 = new ConstructorDemo(10, 20, "Hellow");
+        d1.DisplayData();
+    }
+}

fadd8bd37793bff96ed0351b37805789409a969f
rugvedi <rugvedij1804@gmail.com>
2024-03-13 15:03:06 +0530

    - static variables and methods

--- day10/Encapsulation.md
+++ day10/Encapsulation.md
@@ -1,12 +1,16 @@
 # Encapsulation
 
-1. Wrapping up of data and methods into single unit (class).
-2. we can access variables only through methods (setters & getters)
-3. All variables should be private.
-
 Advantage :- Privacy of data
+
+1. Wrapping up of data and methods into single unit (class).
+2. We can access variables only through methods (setters & getters)
+3. All variables should be private.
 
 ## Static keyword
 
-1. Avoids duplication
-2. Prevents unnecessary memory allocation
+Advantage :- Avoids duplication
+
+1. Prevents unnecessary memory allocation
+2. Static methods can access static stuff directly without creating object
+3. Static methods can access non-static stuff using object
+4. 
---
+++ day10/StaticExample.java
@@ -0,0 +1,42 @@
+package day10;
+
+public class StaticExample {
+
+    static int a = 10; // static variable
+    int b = 20; // non-static variable
+
+    // static method
+    static void m1() {
+        System.out.println("This is m1 static method");
+    }
+
+    // non-static method
+    void m2() {
+        System.out.println("This is m2 non static method");
+    }
+
+    // non static method into method
+    void m() {
+        System.out.println(a); // static
+        System.out.println(b); // non-static
+        m1(); // static
+        m2(); // non-static
+    }
+
+    public static void main(String[] args) {
+        // Static methods can access static stuff directly without creating an object
+        System.out.println(a);
+        // An error will appear - Cannot make a static ref to the non-static field
+        // System.out.println(b);
+        StaticExample se = new StaticExample();
+        System.out.println(se.b);
+
+        m1();
+        // An error will appear - Cannot make a static ref to the non-static method
+        // m2();
+        se.m2();
+
+        se.m();
+
+    }
+}

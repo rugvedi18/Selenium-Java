e7543dde8a47961c538347b5061a1e2771863c2f
rugvedi <rugvedij1804@gmail.com>
2024-06-16 13:27:43 +0530

    - selenium locators and get methods

---
+++ day25/XPathAxes.java
@@ -0,0 +1,19 @@
+package day25;
+// css traverse in DOM form top to bottom in only one direction but
+// XPath can traverse anywhere
+
+public class XPathAxes {
+
+	public static void main(String[] args) {
+		// child
+		// parent
+		// ancestor
+		// Descendant
+		// grand child
+		// grand parent
+		// :first
+		// :last
+
+	}
+
+}
